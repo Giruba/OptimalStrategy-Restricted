52bedb400114e53e1b6185e82cc3b5939623f349
Giruba Beulah SE <gibeulah@microsoft.com>
2019-05-21 08:13:50 +0530

    Add project files.

---
+++ OptimalStrategyRestricted/InputProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptimalStrategyRestricted
+{
+    class InputProcessor
+    {
+        public static int[] GetArrayFromInput() {
+            Console.WriteLine("Enter the number of elements in the array");
+            int[] array = null;
+            try{
+                int sizeOfArray = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the elements separated by space, " +
+                    "comma or semi-colon");
+                String[] elements = Console.ReadLine().Split(' ',',',';');
+                array = new int[sizeOfArray];
+                for (int index = 0; index < sizeOfArray; index++) {
+                    array[index] = int.Parse(elements[index]);
+                }
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+            return array;
+        }
+
+        public static int GetWinnerAmount(int[] array) {
+            int[,] table = new int[array.Length, array.Length];
+
+            for (int index = 0; index < array.Length; index++) {
+                for (int startIndex = 0, endIndex = index; endIndex < array.Length;
+                    startIndex++, endIndex++) {
+                    //Winner takes startIndex, rest of min left by opponent will be Min{startIndex+2, endIndex}
+                    if (startIndex + 2 < endIndex)
+                    {
+                        table[startIndex, endIndex] = array[startIndex] + table[startIndex + 2, endIndex];
+                    }
+                    else {
+                        table[startIndex, endIndex] = array[startIndex];
+                    }
+                    
+                }
+            }
+
+            return table[0, array.Length - 1];
+        }
+    }
+}
---
+++ OptimalStrategyRestricted/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OptimalStrategyRestricted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Optimal Strategy for a game where user can move only from front!");
+            Console.WriteLine("---------------------------------------------------------------");
+
+            int[] array = InputProcessor.GetArrayFromInput();
+            Console.WriteLine("Winner's winning amount[not necessarily winning] is " +
+                InputProcessor.GetWinnerAmount(array));
+
+            Console.ReadLine();
+        }
+    }
+}
